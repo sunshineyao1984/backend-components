8ff2137af20d829c120d71576decd7faaa0dd1ee
sunshineyao1984 <sunshine_yao1984@163.com>
2021-01-26 16:29:56 +0800

    增加sqlserver组件

---
+++ sqlserver-component/src/main/java/com/frog/backend/components/sqlserver/component/mapper/BaseMapper.java
@@ -0,0 +1,14 @@
+package com.frog.backend.components.sqlserver.component.mapper;
+
+import tk.mybatis.mapper.common.IdsMapper;
+import tk.mybatis.mapper.common.Mapper;
+import tk.mybatis.mapper.common.SqlServerMapper;
+
+/**
+ * Description 依赖tk.mybatis框架的通用Mapper
+ *
+ * @author yxy
+ * @date 2021-01-26
+ */
+public interface BaseMapper<T> extends Mapper<T>, IdsMapper<T>, SqlServerMapper<T> {
+}
